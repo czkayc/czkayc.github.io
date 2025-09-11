e46ef695ec0889de6b878f58827f88bd1a1e9ea8
qymljy <xmrx1314mm>
2025-09-11 11:21:13 +0800

    重新部署vue3

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/czkayc.github.io/' : '/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js' // 或具体路径
-    }
-  }
 })
