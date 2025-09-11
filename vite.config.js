6d8078c4b8067e4699c36bf52ef4dd58cfbe6388
qymljy <xmrx1314mm>
2025-09-11 11:12:38 +0800

    重新部署vue3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/czkayc.github.io/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
